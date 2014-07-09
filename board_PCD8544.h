fe79cd896d4a6451d5e235d4d3800b3a46615234
ps <ps@ps-pc.(none)>
2014-07-09 23:09:59 +0300

    finish

---
+++ board_PCD8544.h
@@ -0,0 +1,128 @@
+/*
+ * This file is subject to the terms of the GFX License. If a copy of
+ * the license was not distributed with this file, you can obtain one at:
+ *
+ *              http://ugfx.org/license.html
+ */
+
+/**
+ * @file    drivers/gdisp/PCD8544/board_PCD8544.h
+ * @brief   GDISP Graphic Driver subsystem board interface for the PCD8544 display.
+ */
+
+#ifndef _GDISP_LLD_BOARD_H
+#define _GDISP_LLD_BOARD_H
+
+// For a multiple display configuration we would put all this in a structure and then
+//	set g->board to that structure.
+#define PCD8544_SPI_PORT	GPIOA
+#define PCD8544_SPI_NSS		4
+#define PCD8544_SPI_SCK		5
+#define PCD8544_SPI_MISO	6
+#define PCD8544_SPI_MOSI	7
+
+#define PCD8544_PIN_PORT	GPIOA
+#define PCD8544_PIN_RST		3
+#define PCD8544_PIN_DC		2
+
+#define SET_RST				palSetPad(PCD8544_PIN_PORT, PCD8544_PIN_RST);
+#define CLR_RST				palClearPad(PCD8544_PIN_PORT, PCD8544_PIN_RST);
+
+/*
+ * Maximum speed SPI configuration (18MHz, CPHA=0, CPOL=0, MSb first).
+ */
+static const SPIConfig hs_spicfg = {
+	NULL,
+	PCD8544_SPI_PORT,
+	PCD8544_SPI_NSS,
+	0
+};
+
+#if GFX_USE_OS_CHIBIOS
+	static int32_t thdPriority = 0;
+#endif
+
+static inline void setpin_reset(GDisplay *g, bool_t state) {
+	(void) g;
+	if(state) {
+		CLR_RST;
+	} else {
+		SET_RST;
+	}
+}
+
+static inline void init_board(GDisplay *g) {
+	// As we are not using multiple displays we set g->board to NULL as we don't use it.
+	g->board = 0;
+
+	switch(g->controllerdisplay) {
+	case 0:											// Set up for Display 0
+        /*
+         * SPI1 I/O pins setup.
+         */
+	    palSetPadMode(PCD8544_PIN_PORT, PCD8544_PIN_RST,  PAL_MODE_OUTPUT_PUSHPULL);            /* RESET */
+	    setpin_reset(g, TRUE);
+	    palSetPadMode(PCD8544_PIN_PORT, PCD8544_PIN_DC,   PAL_MODE_OUTPUT_PUSHPULL);            /*  D/C  */
+
+        palSetPadMode(PCD8544_SPI_PORT, PCD8544_SPI_SCK,  PAL_MODE_STM32_ALTERNATE_PUSHPULL);   /* SCK. */
+        palSetPadMode(PCD8544_SPI_PORT, PCD8544_SPI_MISO, PAL_MODE_STM32_ALTERNATE_PUSHPULL);   /* MISO.*/
+        palSetPadMode(PCD8544_SPI_PORT, PCD8544_SPI_MOSI, PAL_MODE_STM32_ALTERNATE_PUSHPULL);   /* MOSI.*/
+        palSetPadMode(PCD8544_SPI_PORT, PCD8544_SPI_NSS,  PAL_MODE_OUTPUT_PUSHPULL);            /* NSS  */
+        palSetPad(PCD8544_SPI_PORT,     PCD8544_SPI_NSS);
+
+		spiInit();
+		break;
+	}
+}
+
+static inline void acquire_bus(GDisplay *g) {
+	(void) g;
+	#if GFX_USE_OS_CHIBIOS
+		thdPriority = (int32_t)chThdGetPriority();
+		chThdSetPriority(HIGHPRIO);
+	#endif
+	spiAcquireBus(&SPID1);
+}
+
+static inline void release_bus(GDisplay *g) {
+	(void) g;
+	#if GFX_USE_OS_CHIBIOS
+		chThdSetPriority(thdPriority);
+	#endif
+	spiReleaseBus(&SPID1);
+}
+
+static inline void write_cmd(GDisplay *g, uint8_t index) {
+	(void)	g;
+
+	palClearPad(PCD8544_PIN_PORT, PCD8544_PIN_DC);
+
+	spiStart(&SPID1, &hs_spicfg);
+	spiSelect(&SPID1);
+	spiStartSend(&SPID1, 1, &index);
+	spiUnselect(&SPID1);
+	spiStop(&SPID1);
+}
+
+static inline void write_data(GDisplay *g, uint8_t data, uint16_t length) {
+	(void) g;
+
+	palSetPad(PCD8544_PIN_PORT, PCD8544_PIN_DC);
+
+	spiStart(&SPID1, &hs_spicfg);
+	spiSelect(&SPID1);
+	spiStartSend(&SPID1, length, &data);
+	spiUnselect(&SPID1);
+	spiStop(&SPID1);
+}
+
+static inline void post_init_board(GDisplay *g) {
+    (void) g;
+}
+
+static inline void set_backlight(GDisplay *g, uint8_t percent) {
+	(void) g;
+	(void) percent;
+}
+
+#endif /* _GDISP_LLD_BOARD_H */
---
+++ main.c
@@ -0,0 +1,155 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "stdlib.h"
+#include "string.h"
+
+#include "ch.h"
+#include "hal.h"
+
+#include "gfx.h"
+
+#define SCB_DEMCR (*(volatile unsigned *)0xE000EDFC)
+#define CPU_RESET_CYCLECOUNTER do { SCB_DEMCR = SCB_DEMCR | 0x01000000; \
+DWT_CYCCNT = 0; \
+DWT_CTRL = DWT_CTRL | 1 ; } while(0)
+
+static int uitoa(unsigned int value, char * buf, int max) {
+    int n = 0;
+    int i = 0;
+    unsigned int tmp = 0;
+
+    if (!buf)
+        return -3;
+
+    if (2 > max)
+        return -4;
+
+    i=1;
+    tmp = value;
+    if (0 > tmp) {
+        tmp *= -1;
+        i++;
+    }
+    for (;;) {
+        tmp /= 10;
+        if (0 >= tmp)
+            break;
+        i++;
+    }
+    if (i >= max) {
+        buf[0] = '?';
+        buf[1] = 0x0;
+        return 2;
+    }
+
+    n = i;
+    tmp = value;
+    if (0 > tmp) {
+        tmp *= -1;
+    }
+    buf[i--] = 0x0;
+    for (;;) {
+        buf[i--] = (tmp % 10) + '0';
+        tmp /= 10;
+        if (0 >= tmp) {
+            break;
+        }
+    }
+    if (-1 != i) {
+        buf[i--] = '-';
+    }
+
+    return n;
+}
+
+void benchmark(void) {
+
+  uint32_t i, pixels, ms, pps;
+  char pps_str[25];
+  coord_t height, width, rx, ry, rcx, rcy;
+  color_t random_color;
+  font_t font;
+
+  //gdispSetOrientation(GDISP_ROTATE_90);
+
+  width = gdispGetWidth();
+  height = gdispGetHeight();
+  font = gdispOpenFont("Archangelsk Regular 12");
+
+  gdispDrawStringBox(0,  0, width, 11, "ChibiOS/GFX", font, White, justifyCenter);
+  gdispDrawStringBox(0, 12, width, 11, "Benchmark", font, White, justifyCenter);
+  gdispDrawStringBox(0, 25, width, 11, "5000 random", font, White, justifyCenter);
+  gdispDrawStringBox(0, 38, width, 11, " rectangles", font, White, justifyCenter);
+  
+  gfxSleepMilliseconds(3000);
+  
+  /* seed for the rand() */
+  srand(DWT_CYCCNT);
+  pixels = 0;
+
+  CPU_RESET_CYCLECOUNTER;
+
+  for (i = 0; i < 5000; i++) {
+    random_color = (rand() % 2);
+    rx  = (rand() % (width   - 10));
+    ry  = (rand() % (height  - 10));
+    rcx = (rand() % ((width  - rx) -10)) + 10;
+    rcy = (rand() % ((height - ry) -10)) + 10;
+
+    gdispFillArea(rx, ry, rcx, rcy, random_color);
+    pixels += (rcx + 1) * (rcy + 1);
+  }
+
+  ms = DWT_CYCCNT / 168000;
+  pps = (float) pixels / ((float) ms / 1000.0f);
+
+  memset (pps_str, 0, sizeof(pps_str));
+  uitoa(pps, pps_str, sizeof(pps_str));
+  strcat(pps_str, " px/s");
+
+  gdispClear(Black);
+  gdispDrawStringBox(0,  0, width, 11, "ChibiOS/GFX", font, White, justifyCenter);
+  gdispDrawStringBox(0, 13, width, 11, "Benchmark", font, White, justifyCenter);
+  gdispDrawStringBox(0, 26, width, 11, pps_str, font, White, justifyCenter);
+  //gdispDrawString(20, height/2, pps_str, font, White);
+}
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
+
+  /* Initialize and clear the display */
+  gfxInit();
+
+  benchmark();
+
+  // Wait forever
+  while(TRUE) {
+    gfxSleepMilliseconds(500);
+  }
+}
